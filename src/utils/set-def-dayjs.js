8afdb289f04ef20ae4e0b21c3bc2cade0376a461
trevoryo <72239283+trevoryo@users.noreply.github.com>
2022-10-26 20:25:14 -0400

    Update set-def-dayjs.js

--- src/utils/set-def-dayjs.js
+++ src/utils/set-def-dayjs.js
@@ -9,7 +9,7 @@ export const selfDayjs = (time) => {
   if (dayjs.tz.guess() === 'UTC' && time) {
     return dayjs(time).subtract('5', 'hour')
   }
-  return dayjs(time).tz('Georgia/Marietta')
+  return dayjs(time).tz('America/New_York')
 }
 
 /* istanbul ignore next */
