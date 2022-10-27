f90c1f4d86442cf0d4038296a44a2a6878cd60c1
trevoryo <72239283+trevoryo@users.noreply.github.com>
2022-10-26 21:10:17 -0400

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -22825,7 +22825,7 @@ export const WEATHER_CITY = [
   {
     id: 3408,
     pid: 0,
-    city_code: '401105100',
+    city_code: '',
     city_name: 'Georgia',
     post_code: null,
     area_code: null,
